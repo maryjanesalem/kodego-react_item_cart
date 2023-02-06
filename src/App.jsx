bfc5811b07bbd91031e10309c0b95cd031da38a3
Jane <maryjanesalem@outlook.ph>
2023-02-06 20:39:32 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+import { useState } from 'react'
+import './css/styles.scss'
+import { FaTrashAlt } from 'react-icons/fa'
+import { FaCaretLeft } from 'react-icons/fa'
+import { FaCaretRight } from 'react-icons/fa'
+
+
+function App() {
+    const [items, setItems] = useState([]);
+
+    const [input, setInput] = useState('');
+    const [totalCount, setTotalCount] = useState(0)
+
+    const AddBtn = () => {
+        const newList = {
+            itemName: input,
+            quantity: 1,
+            isSelected: false
+        }
+        const newLists = [...items, newList];
+
+        setItems(newLists)
+        setInput('');
+    }
+
+    const crossoutText = (index) => {
+        const newLists = [...items];
+        newLists[index].isSelected = !newLists[index].isSelected
+        setItems(newLists)
+    }
+
+    const increment = (index) => {
+        const newLists = [...items];
+        newLists[index].quantity += 1;
+        setItems(newLists)
+        compute()
+    }
+
+    const decrement = (index) => {
+        const newLists = [...items];
+        newLists[index].quantity -= 1;
+        setItems(newLists)
+        compute()
+    }
+
+    const compute = () => {
+        const totalCount = items.reduce((total, item) => {
+            return total + item.quantity
+        }, 0);
+
+        setTotalCount(totalCount);
+    }
+
+
+    // const deleteItem = (item, index) => {
+    //     const newLists = [...items];
+        
+    //     newLists[index] = items.filter(index => item.index !== index) 
+    //     // const newTodoList = todos.filter(todo => todo.id !== id)
+    //     setItems(newLists)
+    // }
+
+
+    return (
+        <div className="App">
+
+            <main>
+                <div className="card text-white bg-secondary mb-3" >
+                    <div className="card-body">
+                        <h4 className="card-title">List App</h4>
+                        <div className="card-text">
+
+                            <div className="form-group d-flex mb-3">
+                                <input value={input} onChange={(e) => setInput(e.target.value)} className="form-control" type="text" placeholder="Write your list here" />
+                                <button onClick={() => AddBtn()} type="button" className="btn btn-primary d-inline">Add</button>
+                            </div>
+
+                            {items.map((item, index) => (
+                                <div className='d-flex lists'>
+                                    <div className='d-flex'>
+                                        <input onClick={() => crossoutText(index)} className="form-check-input checkbox" type="checkbox" />
+                                        {item.isSelected ? (
+                                            <>
+                                                <p className='check-true'>{item.itemName}</p>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <p>{item.itemName}</p>
+                                            </>
+                                        )}
+                                    </div>
+
+                                    <div className='d-flex'>
+                                        <div className='quantity'>
+
+
+                                            <button onClick={() => decrement(index)} type="button" className="btn btn-primary"> <FaCaretLeft /> </button>
+
+                                            <span className='mx-1'>{item.quantity}</span>
+
+                                            <button onClick={() => increment(index)} type="button" className="btn btn-primary"> <FaCaretRight /> </button>
+
+                                        </div>
+                                        {/* <div className='trash'>
+                                            <button onClick={()=> deleteItem(item, index)} type="button" className="btn btn-secondary"><FaTrashAlt /></button>
+                                        </div> */}
+                                    </div>
+                                </div>
+                            ))}
+
+
+                            <hr className='my-2' />
+
+                            <div className='d-flex justify-content-end'>
+                                <h6>Total</h6>
+                                <h6 className='total-display'>{totalCount}</h6>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+        </div>
+    )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import "bootswatch/dist/quartz/bootstrap.min.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
